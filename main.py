688c25913140354b0af2d1a3e8d62419bf7137d1
Praksasreda <praksasreda123@gmail.com>
2026-07-01 23:43:17 +0200

    prototip

---
+++ main.py
@@ -0,0 +1,92 @@
+import cv2
+import threading
+from ultralytics import YOLO
+import time
+import easyocr
+
+class Kamera:
+    def __init__(self,index):
+        self.index = index
+        self.frame = None
+        self.cap = cv2.VideoCapture(index)
+        if not self.cap.isOpened():
+            print(f"Napaka: kamera {index} ni na voljo")
+
+        self.thread = threading.Thread(target=self._update , daemon= True)
+        self.thread.start()
+
+    def _update(self):
+        while True:
+            if self.cap.isOpened():
+                ret,frame = self.cap.read()
+                if ret:
+                    self.frame = frame
+            time.sleep(0.01)
+
+    def release(self):
+        self.cap.release()
+
+def main():
+    model = YOLO("yolov8n.pt")
+    model_tablice = YOLO("license_plate_detector.pt")
+    kamera1 = Kamera(1)
+    reader = easyocr.Reader(['en'])
+    time.sleep(1.0)
+
+
+    while True:
+        trenutni_okvir = kamera1.frame
+        if trenutni_okvir is not None:
+            results = model(trenutni_okvir,verbose = False)
+            
+            #trenutni_okvir = cv2.rectangle(trenutni_okvir,)
+            
+            for box in results[0].boxes:
+                razredId = int(box.cls[0])
+                imeRazred = model.names[razredId]
+                if razredId == 2:
+                    x1,y1,x2,y2 = ([int(e) for e in (box.xyxy[0])])
+
+                    izrez = trenutni_okvir[y1:y2,x1:x2]
+                    #ta izrez je sedaj moja slika moj frame?
+                    izrez_results = model_tablice(izrez,verbose=False)
+                    izrez_box = izrez_results[0].boxes
+                    if len(izrez_box) >0:
+                        tx1,ty1,tx2,ty2 = [int(e) for e in izrez_box.xyxy[0]]
+                        izrezRegisterske = izrez[ty1:ty2,tx1:tx2]
+                        branje_izreza = reader.readtext(izrezRegisterske)
+                        for detection in branje_izreza:
+                            tekst = detection[1]
+                            zaupanje = detection[2]
+                            print(f"tekst:{tekst}, zaupanje {zaupanje}")
+
+                        cv2.rectangle(trenutni_okvir,(x1+tx1,y1+ty1),(x1+tx2,y1+ty2),(255,0,0),2)
+
+                    cv2.rectangle(trenutni_okvir,(x1,y1),(x2,y2),(0,0,255),2)
+
+                    sirina_boxa = x2 - x1
+                    sirina_slike = trenutni_okvir.shape[1]
+
+                    if sirina_boxa > sirina_slike * 0.3:
+                        print("dovolj je blizu")
+                    else :
+                        print("baustela")
+                        
+                    #print(imeRazred)
+            cv2.imshow("prikaz kamere",trenutni_okvir)
+        
+            
+
+
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+        
+    
+    print("zapiram kamero")
+    kamera1.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
