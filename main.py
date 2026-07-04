429a566c54eda47f327cf1101a31f6522146a142
Praksasreda <praksasreda123@gmail.com>
2026-07-04 16:32:09 +0200

    sedaj lahko uporabi vec kamer na enkrat

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import threading
 from ultralytics import YOLO
 import time
 import easyocr
+import numpy as np
 
 class Kamera:
     def __init__(self,index):
@@ -27,25 +28,14 @@ class Kamera:
         self.cap.release()
 
 
-
-def main():
-    model = YOLO("yolov8n.pt")
-    model_tablice = YOLO("license_plate_detector.pt")
-    kamera1 = Kamera(1)
-
-    reader = easyocr.Reader(['en'])
-    time.sleep(1.0)
-
-
-    while True:
-        trenutni_okvir = kamera1.frame
-        if trenutni_okvir is not None:
+def procesirajSliko(trenutni_okvir,model,model_tablice,reader,index):
+    if trenutni_okvir is not None:
             results = model(trenutni_okvir,verbose = False)
-            
-            
+
             for box in results[0].boxes:
                 razredId = int(box.cls[0])
                 imeRazred = model.names[razredId]
+
                 if razredId == 2:
                     x1,y1,x2,y2 = ([int(e) for e in (box.xyxy[0])])
                     sirina_boxa = x2 - x1
@@ -56,6 +46,7 @@ def main():
                         izrez = trenutni_okvir[y1:y2,x1:x2]
                         izrez_results = model_tablice(izrez,verbose=False)
                         izrez_box = izrez_results[0].boxes
+
                         if len(izrez_box) >0:
                             tx1,ty1,tx2,ty2 = [int(e) for e in izrez_box.xyxy[0]]
                             izrezRegisterske = izrez[ty1:ty2,tx1:tx2]
@@ -65,21 +56,60 @@ def main():
                                 tekst = detection[1]
                                 tekst = tekst.replace(" ","")
                                 zaupanje = detection[2]
+
                                 if zaupanje>=0.80 and  7 >= len(tekst) >= 5:
                                     print(f"tekst:{tekst}, zaupanje {zaupanje}")
-
+                                    cv2.putText(trenutni_okvir,tekst,(x1+tx1,y1+ty1+1),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255,0,0),2,cv2.LINE_AA)
+                            #ta okvir je za registersko
                             cv2.rectangle(trenutni_okvir,(x1+tx1,y1+ty1),(x1+tx2,y1+ty2),(255,0,0),2)
-                        
                     
-                    
-            
+                    #ta okvir je za zaznan avto
                     cv2.rectangle(trenutni_okvir,(x1,y1),(x2,y2),(0,0,255),2)
 
-                        
-            cv2.imshow("prikaz kamere",trenutni_okvir)
-        
-            
+            cv2.imshow(f"prikaz kamere {index}",trenutni_okvir)
 
+def dashboard(frames:list):
+    #drugacen pristop potreben
+    h,w = 360,640
+    urejeniOkvirji = [cv2.resize(f,(w,h)) for f in frames]
+
+    while len(frames)<4:
+        urejeniOkvirji.append(np.zeros((h,w,3),dtype=np.uint8))
+
+    vrsta1  = np.hstack([urejeniOkvirji[0],urejeniOkvirji[1]])            
+    vrsta2  = np.hstack([urejeniOkvirji[2],urejeniOkvirji[3]])
+    grid = np.vstack([vrsta1,vrsta2])
+
+    cv2.imshow("dashboard",grid)
+
+
+def najdiKamere(max_index=5)->list:
+    camArr = []
+    for i in range(max_index):
+        cap = cv2.VideoCapture(i)
+        if cap.isOpened():
+            camArr.append(i)
+            cap.release()
+        else:
+            cap.release()
+
+    return camArr
+
+
+
+
+def main():
+    model = YOLO("yolov8n.pt")
+    model_tablice = YOLO("license_plate_detector.pt")
+    aktivneKamere = [Kamera(i) for i in najdiKamere()]
+    reader = easyocr.Reader(['en'])
+    time.sleep(1.0)
+
+
+    while True:
+        for index in range(len(aktivneKamere)):
+            trenutni_okvir = aktivneKamere[index].frame
+            procesirajSliko(trenutni_okvir,model,model_tablice,reader,index)
 
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -87,7 +117,8 @@ def main():
         
     
     print("zapiram kamero")
-    kamera1.release()
+    for kamera in aktivneKamere:
+        kamera.release()
     cv2.destroyAllWindows()
 
 
